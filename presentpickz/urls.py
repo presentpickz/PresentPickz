9fb018b297a6b63a2d6a60add66a596dface5b3a
Ashif <bashajafer36@gmail.com>
2026-03-29 20:29:03 +0530

    Add Google Search Console verification file and route

--- presentpickz/urls.py
+++ presentpickz/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
+from django.http import HttpResponse
 
 urlpatterns = [
     # Admin MUST be first to prevent catch-all patterns from intercepting
@@ -16,6 +17,9 @@ urlpatterns = [
     path('users/', include('users.urls')),
     path('accounts/', include('allauth.urls')),
     
+    # Google Verification File (for Search Console)
+    path('googleccfc70bcd8a6f5fe.html', lambda r: HttpResponse("google-site-verification: googleccfc70bcd8a6f5fe.html", content_type="text/html")),
+
     # ALWAYS serve media files (works in debug AND production)
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
